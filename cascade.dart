158817df72071cdd8723ef68ebcdea9bcc4df8a7
Gtigerbd <ektigerbd@gmail.com>
2025-11-01 18:53:37 +0300

    abav

---
+++ cascade.dart
@@ -0,0 +1,5 @@
+void main() {
+  int a = 10;
+  int s = 20;
+  print(a + s);
+}
---
+++ kaha.dart
@@ -0,0 +1,7 @@
+void main() {
+  String str1 = "apple";
+
+  //String str2 = "banana";
+  String str3 = "apple ";
+  print(str1 == str3);
+}
